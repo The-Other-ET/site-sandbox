2dfdbf21f0a76aa70e0251c8015416161a2af03f
Thomas,Eric <Eric.Thomas@Cerner.com>
2025-03-25 16:51:28 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.engine('html', require('ejs').renderFile)
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.render('home.html')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
