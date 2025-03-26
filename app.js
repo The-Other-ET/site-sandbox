90023e2afc50e9a317e415888b3fa5f9621cd961
Eric Thomas <elthomas7@outlook.com>
2025-03-26 16:17:26 -0500

    update some branding, content

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.engine('html', require('ejs').renderFile)
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
     res.render('index.html')
