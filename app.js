906e7c31bc2644eceb852a153470e82ba6a74e63
Eric Thomas <elthomas7@outlook.com>
2025-03-29 16:03:54 -0500

    added some preliminary template work

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 3000
 app.engine('html', require('ejs').renderFile)
 app.use(express.static(__dirname))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
 })
 
 app.listen(port, () => {
