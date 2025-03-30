7b595bb8156af1d9210450843978e49533e38764
Eric Thomas <elthomas7@outlook.com>
2025-03-30 11:26:40 -0500

    updated links for github support

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const app = express()
 const port = 3000
 app.engine('html', require('ejs').renderFile)
 app.use(express.static(__dirname))
-app.set('views', __dirname)
+app.set('/', __dirname)
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index.html')
 })
 
 app.get('/about', (req, res) => {
-    res.render('/views/about')
+    res.render('about.html')
 })
 
 app.listen(port, () => {
